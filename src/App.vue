c74a2fc070cc477ef9ac7b3bea9498cc33b0a70a
Mark Manders <mmjmanders@gmail.com>
2026-01-02 13:40:50 +0100

    feat: add navigation and routes for top tracks, artists, and albums
    - Created `TopTracks.vue`, `TopArtists.vue`, and `TopAlbums.vue` components.
    - Added routes for the new components with navigation menu in the header.
    - Updated router to include default redirect to `/top-tracks`.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
+import { routes } from '@/router'
 </script>
 
 <template>
+  <header class="w-full bg-neutral-100">
+    <nav class="h-14 w-full lg:w-1/2 lg:mx-auto flex justify-start items-center px-4">
+      <RouterLink
+        :to="route.path"
+        role="navigation"
+        v-for="route in routes"
+        :key="route.path"
+        activeClass="text-neutral-900 bg-neutral-200"
+        class="px-4 flex items-center h-full text-neutral-500 hover:text-neutral-900"
+        >{{ route.name }}</RouterLink
+      >
+    </nav>
+  </header>
+  <main class="m-4">
+    <RouterView />
+  </main>
   <VueQueryDevtools />
 </template>
 
